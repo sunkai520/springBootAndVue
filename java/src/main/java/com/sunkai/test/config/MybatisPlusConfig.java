0516bb4bb364820bb133efe7807e23e13a7dd54c
sunkai <sunkai@anming.tech>
2022-02-16 14:42:43 +0800

    jj

--- java/src/main/java/com/sunkai/test/config/MybatisPlusConfig.java
+++ java/src/main/java/com/sunkai/test/config/MybatisPlusConfig.java
@@ -3,8 +3,13 @@ package com.sunkai.test.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
 /**
  * 配置mybatisPlus分页插件
  * 若不配置 分页会无效
@@ -16,5 +21,15 @@ public class MybatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+    /**
+     * 设置文件上传最大10M
+     * */
+    @Bean
+    public MultipartConfigElement multipartConfigElement(){
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(10));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(10));
+        return factory.createMultipartConfig();
     }
 }
